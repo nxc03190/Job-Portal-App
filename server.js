d45c7f3d620325521688c946f74806775b9a2993
nxc03190 <134020121+nxc03190@users.noreply.github.com>
2023-06-17 00:29:26 -0500

    Updated DB code

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ const connection = mysql.createConnection({
   host: 'localhost', // Replace with your MySQL host
   user: 'root', // Replace with your MySQL username
   password: 'root', // Replace with your MySQL password
-  database: 'myjobportaldb', // Replace with your MySQL database name
+  //database: 'myjobportaldb', // Replace with your MySQL database name
 });
 
 // Connect to MySQL
@@ -59,39 +59,94 @@ connection.connect((error) => {
 
 // Define your API endpoint for handling form data insertion
 app.post('/submitApplication', (req, res) => {
-    const userData = req.body;
-    console.log('---------------------------');
-    console.log(userData);
-  
-    // Insert user data into the MySQL database
-    const query = 'INSERT INTO users (firstname, lastname, email, Phone, password, gender, birthday, quantity, language1, color, login) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-    const values = [
-      userData.firstname,
-      userData.lastname,
-      userData.email,
-      userData.Phone,
-      userData.password,
-      userData.gender,
-      userData.birthday,
-      userData.quantity,
-      userData.language1,
-      userData.color,
-      userData.login
-    ];
+  const userData = req.body;
+  console.log('---------------------------');
+  console.log(userData);
 
-    console.log(values)
-    console.log('Values from Server.js file')
-    connection.query(query, values, (error, results) => {
-      if (error) {
-        console.error('Error inserting user data:', error);
-        res.status(500).json({ error: 'Failed to insert user data' });
-      } else {
-        console.log('User data inserted successfully');
-        res.status(200).json({ message: 'User data inserted successfully' });
-      }
-    });
+  // Create the database if it doesn't exist
+  const createDatabaseQuery = `CREATE DATABASE IF NOT EXISTS mydatabase`;
+  connection.query(createDatabaseQuery, (databaseError) => {
+    if (databaseError) {
+      console.error('Error creating database:', databaseError);
+      res.status(500).json({ error: 'Failed to create database' });
+    } else {
+      console.log('Database created or already exists');
+
+      // Select the database to use
+      const useDatabaseQuery = `USE mydatabase`;
+      connection.query(useDatabaseQuery, (useDatabaseError) => {
+        if (useDatabaseError) {
+          console.error('Error using database:', useDatabaseError);
+          res.status(500).json({ error: 'Failed to use database' });
+        } else {
+          console.log('Using database');
+
+          // Create user_data table if it doesn't exist
+          const createTableQuery = `CREATE TABLE IF NOT EXISTS user_data (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            firstname VARCHAR(50),
+            lastname VARCHAR(50),
+            email VARCHAR(100),
+            phone VARCHAR(15),
+            gender VARCHAR(10),
+            birthday DATE,
+            age INT,
+            job VARCHAR(100),
+            language VARCHAR(100),
+            degree VARCHAR(100),
+            job_preference VARCHAR(100),
+            color VARCHAR(7),
+            university VARCHAR(100),
+            previousExperience TEXT
+          )`;
+
+          connection.query(createTableQuery, (error) => {
+            if (error) {
+              console.error('Error creating user_data table:', error);
+              res.status(500).json({ error: 'Failed to create user_data table' });
+            } else {
+              console.log('user_data table created or already exists');
+
+              // Insert user data into the user_data table
+              const insertQuery = 'INSERT INTO user_data (firstname, lastname, email, phone, gender, birthday, age, job, language, degree, job_preference, color, university, previousExperience) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+              const values = [
+                userData.firstname,
+                userData.lastname,
+                userData.email,
+                userData.phone,
+                userData.gender,
+                userData.birthday,
+                userData.age,
+                userData.job,
+                userData.language,
+                userData.degree,
+                userData.job_preference,
+                userData.color,
+                userData.university,
+                userData.previousExperience
+              ];
+
+              console.log(values);
+              console.log('Values from Server.js file');
+              connection.query(insertQuery, values, (insertError, results) => {
+                if (insertError) {
+                  console.error('Error inserting user data:', insertError);
+                  res.status(500).json({ error: 'Failed to insert user data' });
+                } else {
+                  console.log('User data inserted successfully');
+                  res.status(200).json({ message: 'User data inserted successfully' });
+                }
+              });
+            }
+          });
+        }
+      });
+    }
   });
-  
+});
+
+
+
 
 // Start the server
 const port = 3000; // Replace with your desired port number
